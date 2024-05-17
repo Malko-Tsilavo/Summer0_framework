3fd8d40e53f61f92ae168ee831ddee95cae9fc5d
Malko-Tsilavo <157007415+Malko-Tsilavo@users.noreply.github.com>
2024-05-17 11:22:17 +0300

    Add files via upload

---
+++ sprint1_framework/src/mg/sprint/Annotation/Annotation_controller.java
@@ -0,0 +1,12 @@
+package mg.sprint.Annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Annotation_controller
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Annotation_controller {
+
+}
