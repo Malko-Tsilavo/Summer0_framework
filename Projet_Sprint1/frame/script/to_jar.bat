a5ec839b497a9f1e240e782f7e9cf475d0da1066
Malko-Tsilavo <157007415+Malko-Tsilavo@users.noreply.github.com>
2024-05-17 11:02:50 +0300

    Add files via upload

---
+++ Projet_Sprint1/frame/script/to_jar.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "work-dir=E:\ITU\S4\dynamique_web\Projet_Sprint1\frame"
+set "src=%work-dir%\src"
+set "bin=%work-dir%\bin"
+set "lib=%work-dir%\lib"
+
+
+
+echo Demarage Jar
+jar cf "%work-dir%\lib\sprint1.jar" -C "%bin%" .
+echo Fin Jar
